b2d0ef27fc2156332bbe2d35b4a1e9742fae0e8e
D0lph1n25 <d0lph1n.25_119@outlook.jp>
2023-11-02 18:10:17 +0900

    単体テスト実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :nickname, presence: true
-  validates :email, presence: true
-  validates :password, presence: true, length: {minimum: 6}, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[A-Za-z\d]+\z/}
+  validates :nickname, presence: true,  length: {minimum: 6}
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
   validates :birthday, presence: true
+
 end
