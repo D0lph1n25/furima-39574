27783d78475cf317f88bc4a068bc5f88279fd1f0
D0lph1n25 <d0lph1n.25_119@outlook.jp>
2023-11-09 20:19:42 +0900

    Java Script　実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,18 +10,16 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      @price = @item.item_price
-      @fee = @price*0.1
       redirect_to root_path      
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
   private 
 
   def item_params
-    params.require(:item).permit(:content, :item_image, :item_price).merge(user_id: current_user.id)
+    params.require(:item).permit(:item_image, :item_image, :item_name, :item_info, :item_category_id, :item_sales_status_id, :item_shipping_fee_status_id, :prefecture_id, :item_scheduled_delivery_id, :item_price).merge(user_id: current_user.id)
   end
   
 end
