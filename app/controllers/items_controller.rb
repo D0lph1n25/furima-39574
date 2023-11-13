bfdd6baf695c2d643409331115c441cc40f25441
D0lph1n25 <d0lph1n.25_119@outlook.jp>
2023-11-13 12:59:00 +0900

    rubocop　実施

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,10 @@
 class ItemsController < ApplicationController
   def index
-   @items = Item.order(created_at: :desc)
+    @items = Item.order(created_at: :desc)
   end
 
   def new
-    if  user_signed_in?
+    if user_signed_in?
       @item = Item.new
     else
       redirect_to new_user_session_path
@@ -23,6 +23,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:item_image, :item_name, :item_info, :item_category_id, :item_sales_status_id,:item_shipping_fee_status_id, :prefecture_id, :item_scheduled_delivery_id, :item_price).merge(user_id: current_user.id)
+    params.require(:item).permit(:item_image, :item_name, :item_info, :item_category_id, :item_sales_status_id,
+                                 :item_shipping_fee_status_id, :prefecture_id, :item_scheduled_delivery_id, :item_price).merge(user_id: current_user.id)
   end
 end
