8a700e99bcc7e48c663055ae28be2d4867a9f476
D0lph1n25 <d0lph1n.25_119@outlook.jp>
2023-11-14 20:40:46 +0900

    before_action　設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_session, only: [:new, :edit]
+  before_action :item_find, only: [:show, :edit, :update]
   
   def index
     @items = Item.order(created_at: :desc)
@@ -19,18 +20,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     unless user_signed_in? && (current_user.id == @item.user_id)
       redirect_to root_path
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -49,6 +47,10 @@ class ItemsController < ApplicationController
     unless user_signed_in?
       redirect_to new_user_session_path
     end
+  end
+
+  def item_find
+    @item = Item.find(params[:id])
   end
 
 end
