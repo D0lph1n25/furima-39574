a4af290e3e71c29226388c5bfc4dc07b16353f31
D0lph1n25 <d0lph1n.25_119@outlook.jp>
2023-11-14 21:39:08 +0900

    Revert "destroyアクション　実装"
    This reverts commit 99e8b3ecdc880abe4b26f096d5ed25429e93ad9e.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_session, only: [:new, :edit]
-  before_action :item_find, only: [:show, :edit, :update, :destroy]
+  before_action :item_find, only: [:show, :edit, :update]
   
   def index
     @items = Item.order(created_at: :desc)
@@ -33,13 +33,6 @@ class ItemsController < ApplicationController
       redirect_to item_path(@item.id)
     else
       render :edit, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    if user_signed_in? && current_user.id == @item.user_id
-      @item.destroy
-      redirect_to root_path
     end
   end
 
