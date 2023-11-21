c97610ccf24fa3b63e5d3217d1d4fc52c7a1e0f3
D0lph1n25 <d0lph1n.25_119@outlook.jp>
2023-11-21 23:30:52 +0900

    OrderShippingAddressモデル作成

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -3,12 +3,4 @@ class ShippingAddress < ApplicationRecord
   belongs_to :prefecture
   belongs_to :order
 
-  with_options presence: true do
-    validates :postal_code
-    validates :city
-    validates :addresses
-    validates :phone_number
-    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"}
-  end
-
 end
