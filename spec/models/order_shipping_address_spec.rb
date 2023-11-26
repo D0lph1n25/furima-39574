aab3d3478369ac636b5338b706c15111f685eaba
D0lph1n25 <d0lph1n.25_119@outlook.jp>
2023-11-26 16:43:03 +0900

    order_shipping_addressテスト　修正

--- spec/models/order_shipping_address_spec.rb
+++ spec/models/order_shipping_address_spec.rb
@@ -21,52 +21,65 @@ RSpec.describe OrderShippingAddress, type: :model do
   end
 
   context '登録できないとき' do
-    it '郵便番号が入力されていない' do
+    it '郵便番号が入力されていないと登録できない' do
       @order_shipping_address.postal_code = ""
       @order_shipping_address.valid?
       expect(@order_shipping_address.errors.full_messages).to include("Postal code can't be blank")
     end
 
-    it '郵便番号が3桁ハイフン4桁の形式で入力されていない' do
+    it '郵便番号が3桁ハイフン4桁の形式で入力されていないと登録できない' do
       @order_shipping_address.postal_code = "1234456"
       @order_shipping_address.valid?
       expect(@order_shipping_address.errors.full_messages).to include("Postal code [3桁ハイフン4桁]の形式で入力してください")
     end
 
-    it '都道府県が選択されていない（---の状態である）' do
+    it '都道府県が選択されていない（---の状態である）と登録できない' do
       @order_shipping_address.prefecture_id = 1
       @order_shipping_address.valid?
       expect(@order_shipping_address.errors.full_messages).to include("Prefecture can't be blank")
     end
 
-    it '市区町村が入力されていない' do
+    it '市区町村が入力されていないと登録できない' do
       @order_shipping_address.city = ""
       @order_shipping_address.valid?
       expect(@order_shipping_address.errors.full_messages).to include("City can't be blank")
     end
 
-    it '番地が入力されていない' do
+    it '番地が入力されていないと登録できない' do
       @order_shipping_address.addresses = ""
       @order_shipping_address.valid?
       expect(@order_shipping_address.errors.full_messages).to include("Addresses can't be blank")
     end
 
-    it '電話番号が入力されていない' do
+    it '電話番号が入力されていないと登録できない' do
       @order_shipping_address.phone_number = ""
       @order_shipping_address.valid?
       expect(@order_shipping_address.errors.full_messages).to include("Phone number can't be blank")
     end
 
-    it '電話番号が１０桁以上１１桁以内でない' do
+    it '電話番号が１０桁以上１１桁以内でないと登録できない' do
       @order_shipping_address.phone_number = "090-1234-5678"
       @order_shipping_address.valid?
       expect(@order_shipping_address.errors.full_messages).to include("Phone number 10桁以上11桁以内の半角数値で入力してください")
     end
 
-    it '電話番号が半角数値ではないとき' do
+    it '電話番号が半角数値ではないと登録できない' do
       @order_shipping_address.phone_number = "０９０１２３４５６７８"
       @order_shipping_address.valid?
       expect(@order_shipping_address.errors.full_messages).to include("Phone number 10桁以上11桁以内の半角数値で入力してください")
     end
+
+    it 'userが紐付いていないと登録できない' do
+      @order_shipping_address.user_id = nil
+      @order_shipping_address.valid?
+      expect(@order_shipping_address.errors.full_messages).to include("User can't be blank")
+    end
+
+    it 'itemが紐付いていないと登録できない'
+      @order_shipping_address.item_id = nil
+      @order_shipping_address.valid?
+      expect(@order_shipping_address.errors.full_messages).to include("Item can't be blank")
+    end
+    
   end
 end
