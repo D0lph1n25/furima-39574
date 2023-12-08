dfdd7edf63e69a2303fb1d31e0d81acb1b3486dc
D0lph1n25 <d0lph1n.25_119@outlook.jp>
2023-12-08 19:03:24 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -40,15 +40,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_21_132746) do
   end
 
   create_table "items", charset: "utf8mb4", force: :cascade do |t|
-    t.string "item_name"
-    t.text "item_info"
-    t.integer "item_category_id"
-    t.integer "item_sales_status_id"
-    t.integer "item_shipping_fee_status_id"
-    t.integer "prefecture_id"
-    t.integer "item_scheduled_delivery_id"
-    t.integer "item_price"
-    t.bigint "user_id"
+    t.string "item_name", null: false
+    t.text "item_info", null: false
+    t.integer "item_category_id", null: false
+    t.integer "item_sales_status_id", null: false
+    t.integer "item_shipping_fee_status_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "item_scheduled_delivery_id", null: false
+    t.integer "item_price", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_items_on_user_id"
